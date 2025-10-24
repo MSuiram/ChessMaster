4afc7877f09af53372b3cd72e1d0d6d72e5c9b7b
MSuiram <marius.valard@gmail.com>
2025-10-24 11:38:03 +0200

    Add Person and Player

--- ChessMaster/Models/PersonModel.cs
+++ ChessMaster/Models/PersonModel.cs
@@ -1,24 +1,41 @@
 
+using System;
 using System.Security.Authentication.ExtendedProtection;
 using Avalonia.Controls;
 
 public class Person
 {
-    private int id_player;
+    private long id_player;
     private string first_name;
     private string last_name;
-    private int age;
+    private DateOnly born_date;
 
-    public Person(string first_name, string last_name, int age)
+    public Person(string first_name, string last_name, DateOnly born_date)
     {
-        this.id_player = 0;
+        this.id_player = Convert.ToInt64(DateTime.Now.ToString("yyyymmddhhmmss"));
         this.first_name = first_name;
         this.last_name = last_name;
-        this.age = age;
+        this.born_date = born_date;
     }
 
-    public void Change_first_name()
+    public void Change_first_name(string new_name)
     {
-
+        first_name = new_name;
     }
+
+    public void Change_last_name(string new_name)
+    {
+        last_name = new_name;
+    }
+
+    public void Change_born_date(DateOnly new_born_date)
+    {
+        born_date = new_born_date;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("Id : {0}, Firstname : {1}, Lastname : {2}, Born date : {3}", id_player, first_name, last_name, born_date);
+    }
+
 }
