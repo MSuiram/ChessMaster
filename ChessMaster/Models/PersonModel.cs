3fc2401a8e21e0f5eda086db97bfc144c34c24bd
MSuiram <marius.valard@gmail.com>
2025-10-24 10:01:54 +0200

    Add gitignore

---
+++ ChessMaster/Models/PersonModel.cs
@@ -0,0 +1,18 @@
+
+public class Person
+{
+    private int id_player;
+    private string first_name;
+    private string last_name;
+    private int age;
+
+    public Person(string first_name, string last_name, int age)
+    {
+        this.id_player = 0;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.age = age;
+    }
+
+    public 
+}
