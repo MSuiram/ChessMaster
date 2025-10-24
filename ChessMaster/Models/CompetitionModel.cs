34487d67fbcfb7266ec317d673df357d04727b0c
TheNear <tomtom.leman@gmail.com>
2025-10-24 10:02:37 +0200

    ModelCreation_compet

---
+++ ChessMaster/Models/CompetitionModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+public class Competition
+{
+    private string Name;
+    private int ID;
+    private List<PlayerModel> participants = new List<PlayerModel>();
+    private List<MatchModel> matches = new List<MatchModel>();
+    
+}
--- ChessMaster/obj/Debug/net9.0/ChessMaster.AssemblyInfo.cs
+++ ChessMaster/obj/Debug/net9.0/ChessMaster.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessMaster")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85093f804e7bacb65d6299ea6ee485bd13acb314")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+841cce1311b64fb56545013c2bcaaae15e1ce90e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessMaster")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessMaster")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
