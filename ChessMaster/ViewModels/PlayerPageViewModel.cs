40aeb3c7f401c72944775eff61e143f363a7b762
MSuiram <marius.valard@gmail.com>
2025-11-21 10:19:02 +0100

    Link Form in PlayerPage

--- ChessMaster/ViewModels/PlayerPageViewModel.cs
+++ ChessMaster/ViewModels/PlayerPageViewModel.cs
@@ -1,6 +1,29 @@
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reflection.Metadata.Ecma335;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+
 namespace ChessMaster.ViewModels;
 
 public partial class PlayerPageViewModel : ViewModelBase
 {
-    public string Test { get; set; } = "Player";
+
+    [ObservableProperty]
+    private string? _lastname;
+    [ObservableProperty]
+
+    private string? _firstname;
+    [ObservableProperty]
+
+    private string? _id;
+
+    [ObservableProperty]
+    private string? _test = "e";
+
+    [RelayCommand]
+    private void GetName() => Test = Lastname;
+
 }
